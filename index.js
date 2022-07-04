31ca35131c82fae51853d2698fefc99f84570a5a
FabiMi <91958341+FabiMi@users.noreply.github.com>
2022-07-04 16:59:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const { check, validationResult } = require('express-validator');
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8090', 'http://testsite.com' ,'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8090' ,'http://localhost:1234'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
