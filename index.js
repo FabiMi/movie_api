8864e05065c8dd703466fe35a51e203db9337f34
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-07-26 13:46:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
-      //If a specific origin isn't found on the list of alloed origins
+      //If a specific origin isn't found on the list of alloed origins !
       let message = 'The CORS policy for this application doesnt allow acces from origin' + origin;
       return callback(new Error(message), false);
     }
